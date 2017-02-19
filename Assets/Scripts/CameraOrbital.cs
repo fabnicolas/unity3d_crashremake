fff04bbd770139c7af0abbb141e354cdf74c9196
Finalgalaxy <finalgalaxy@gmail.com>
2017-02-19 16:21:13 +0100

    Init repository. Initial state upload.

---
+++ Assets/Scripts/CameraOrbital.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class CameraOrbital : MonoBehaviour {
+
+	private float vertical;
+	private float rotation_speed = 4.0f;
+	void Start ()
+	{
+		vertical = transform.eulerAngles.x;
+	}
+
+	void Update ()
+	{
+		var mouseVertical = Input.GetAxis("Mouse Y");
+		vertical = (vertical - rotation_speed * mouseVertical) % 360f;
+		vertical = Mathf.Clamp(vertical, -30, 60);
+		transform.localRotation = Quaternion.AngleAxis(vertical, Vector3.right);
+	}
+}
