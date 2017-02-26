af4112f7e58231ba2b531d6f296317855bbb2609
Finalgalaxy <finalgalaxy@gmail.com>
2017-02-26 18:38:49 +0100

    Updated player movements through coroutines for better performances.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour{
+    // Still not used, work in progress...
 	public enum PlayerMovementStatus : byte{
         IDLE = 0,
 		WALKING = 1,
@@ -10,17 +12,15 @@ public class PlayerController : MonoBehaviour{
 		WALKING_LEFT = 5
 	}
 
-    private PlayerMovementStatus movement_status;
-    private Animator _animator;
+    private PlayerMovementStatus movement_status;   // Work in progress...
+    private Animator _animator;    // For animations.
 
-    private Vector3 movement_vector;
+    private Vector3 movement_vector;    // For movements. Idle: (0,0,0).
 
-    private float horizontal;
+    private float horizontal;   // Horizontal rotation for mouse look.
     
-    /// <summary>
     /// Start is called on the frame when a script is enabled just before
     /// any of the Update methods is called the first time.
-    /// </summary>
     void Start()
     {
         /*
@@ -30,13 +30,12 @@ public class PlayerController : MonoBehaviour{
 		position_player.y = 0.5f;
 		position_player.z = 0f;
         */
-        _animator=this.GetComponent<Animator>(); // Gets the animator to the attached GO.
-        horizontal = transform.eulerAngles.y;
+        _animator=this.GetComponent<Animator>(); // Gets the animator from the attached GO.
+        horizontal = transform.eulerAngles.y;    // Sets rotation from the actual focus-rotation of the player based on Y axis.
+        isMovementCoroutineRunning=false;        // Sets running=false to enable movement coroutine calls.
     }
 
-    /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
-    /// </summary>
     void Update()
     {
         //movement_vector = Vector3.zero;
@@ -59,20 +58,35 @@ public class PlayerController : MonoBehaviour{
         }
     }
 
-    /// <summary>
     /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
-    /// </summary>
     void FixedUpdate()
     {
         float mouseHorizontal = Input.GetAxis("Mouse X");
-		horizontal = (horizontal + 0.4f * mouseHorizontal) % 360f;
+		horizontal = (horizontal + 4f * mouseHorizontal) % 360f;
 		transform.rotation = Quaternion.AngleAxis(horizontal, Vector3.up); // Handles mouse X orientation
         
-        Debug.Log("mv="+movement_vector);
-        if(movement_vector != Vector3.zero)
-            transform.Translate((movement_vector=movement_vector*Time.deltaTime));
-        else
-            transform.Translate(0,0,0);
+        if(movement_vector!=Vector3.zero && !isMovementCoroutineRunning){
+            StartCoroutine(executeMovement()); // Delegate movement to 'async' coroutine function to save performance.
+        }
+    }
+
+    /*
+        executeMovement is a coroutine called from FixedUpdate method to manage efficiently character movement.
+        It relies on movement_vector value (set from Update() function).
+    */
+    private bool isMovementCoroutineRunning; // This value estabilishes if coroutine is already called.
+    IEnumerator executeMovement(){
+        // Coroutine started running, so the movement vector is != (0,0,0). 'Callback' through setting bool to true.
+        isMovementCoroutineRunning=true;
+
+        // Until the movement vector is not (0,0,0), execute the movement each FixedUpdate frame.
+        while(movement_vector != Vector3.zero){
+            transform.Translate((movement_vector=movement_vector*Time.deltaTime));  // Make movement and reduce movement_vector on deltaTime.
+            yield return CoRoutineWaitBuilder.getInstance().m_WaitForFixedUpdate;   // Wait next frame elaboration.
+        }
+
+        // Character speed is zero. Coroutine is already going to die. Let's 'callback' through setting bool to false.
+        isMovementCoroutineRunning=false;
     }
 
 
