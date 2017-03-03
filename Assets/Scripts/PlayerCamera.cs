d5fc309a7a1609b0ba459e8f7eb7691743fd3bde
Finalgalaxy <finalgalaxy@gmail.com>
2017-03-03 15:02:04 +0100

    Test VSCode indenting.

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,24 +1,25 @@
 using UnityEngine;
 
-public class PlayerCamera : MonoBehaviour{
+public class PlayerCamera : MonoBehaviour
+{
     public Transform target_transform;
-	private float vertical;
-	private float rotation_speed = 4.0f;
+    private float vertical;
+    private float rotation_speed = 4.0f;
 
     private Vector3 camera_offset;
-	void Start ()
-	{
-		vertical = transform.eulerAngles.x;
+    void Start()
+    {
+        vertical = transform.eulerAngles.x;
         camera_offset = transform.position;
-	}
+    }
 
-	void Update ()
-	{
-        transform.position = target_transform.position+camera_offset;
+    void Update()
+    {
+        transform.position = target_transform.position + camera_offset;
 
-		float mouseVertical = Input.GetAxis("Mouse Y");
-		vertical = (vertical - rotation_speed * mouseVertical) % 360f;
-		vertical = Mathf.Clamp(vertical, -5, 20);
-		transform.localRotation = Quaternion.AngleAxis(vertical, Vector3.right);
-	}
+        float mouseVertical = Input.GetAxis("Mouse Y");
+        vertical = (vertical - rotation_speed * mouseVertical) % 360f;
+        vertical = Mathf.Clamp(vertical, -5, 20);
+        transform.localRotation = Quaternion.AngleAxis(vertical, Vector3.right);
+    }
 }
